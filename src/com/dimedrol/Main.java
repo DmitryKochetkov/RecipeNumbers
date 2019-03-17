b174d8dbc26f6da56094b075b275d593ae17659d
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-03-17 14:39:40 +0300

    Three digit numbers base
    Начало положено

--- src/com/dimedrol/Main.java
+++ src/com/dimedrol/Main.java
@@ -7,9 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int x = s.nextInt();
-
-        RecipeNumber r = new RecipeNumber(x);
-        System.out.print(r.recipe);
+        RecipeNumber r;
+        for (int i = 100; i < 130; i++)
+        {
+            r = new RecipeNumber(i);
+            System.out.println(i + " " + r.recipe);
+        }
     }
 }
--- src/com/dimedrol/RecipeNumber.java
+++ src/com/dimedrol/RecipeNumber.java
@@ -2,9 +2,38 @@ package com.dimedrol;
 
 public class RecipeNumber {
     int num;
-    String recipe;
+    String recipe = "";
+
+    String[] num0_masc = {"один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+    String[] num0_fem = {"одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+    String[] num1 = {"десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
+    String[] num2_masc = {"сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
+    String[] degree = {"тысяча", "миллион"};
 
     public RecipeNumber(int num) {
         this.num = num;
+        int digit0;
+        int digit1;
+        int digit2;
+
+        for (int i = 0; i < Integer.toString(num).length() / 3; i++) {
+            int buf = num % 1000;
+            digit0 = buf % 10;
+            digit1 = buf / 10 % 10;
+            digit2 = buf / 100;
+
+            if (digit2 > 0)
+                recipe += num2_masc[digit2 - 1];
+
+            if (digit1 == 1 && digit0 > 0)
+                recipe += " " + num0_masc[digit0 - 1] + "надцать";
+            else {
+                if (digit1 > 1)
+                    recipe += " " + num1[digit1 - 1];
+                if (digit0 > 0)
+                    recipe += " " + num0_masc[digit0 - 1];
+                num /= 1000;
+            }
+        }
     }
 }
